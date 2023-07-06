a95c52afc76792b241738cdbd8655154bfc7e0b1
Martin <574641@stud.hvl.no>
2023-07-06 15:06:50 +0200

    Små endringer

--- Age-Calculator/age_calculator.js
+++ Age-Calculator/age_calculator.js
@@ -6,17 +6,34 @@ hente inputs (DAY MONTH YEAR) fra bruker
 
 validere inputs:
 - tom input
-- ugyldig input (over 31 day, 12 month, 2023 year, under 0)
+- ugyldig input (over 31 day, 12 month, 2023 year)
+- ugyldig input (date in future)
+- ugyldig input (not valid date in calendar)
 
 kalkulere alder (DAYS MONTHS YEARS)
 
 sende variabler til HTML
 
-vise feilmeldinger
-
+sende eventuelle feilmeldinger
 
 */
 
+// Inputs:
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+
 const button = document.querySelector("button");
+
+// Metode for å kalkulere alder
+function calculate() {
+    let invalidInput = false;
+    validateInput(day, month, year);
+}
+
+// Metode for å validere input
+function validateInput () {
+    
+}
 
 button.addEventListener('click', calculate);
